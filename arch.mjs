c7f442ee28c974bde22038399328c4b2df8c72a5
hopejr <hopejr@gmail.com>
2023-11-24 08:18:06 +1100

    Cross-compile for x64

--- arch.mjs
+++ arch.mjs
@@ -1,6 +1,13 @@
 import spawn from "cross-spawn"
 
-spawn.sync("npm", ["run", process.platform === "linux" && process.env.npm_config_target_arch === "arm" ? "build:arm" : "build"], {
-  input: "Native module build required.",
-  stdio: "inherit"
-})
+if (process.platform === "linux") {
+  spawn.sync("npm", ["run", process.env.npm_config_target_arch === "arm" ? "build:arm" : process.env.npm_config_target_arch === "x64" ? "build:linuxx86" : "build"], {
+    input: "Native module build required.",
+    stdio: "inherit"
+  })
+} else {
+  spawn.sync("npm", ["run", "build"], {
+    input: "Native module build required.",
+    stdio: "inherit"
+  })
+}
