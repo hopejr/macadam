09f9517384c01b0b28680ce840ae37be7f112b28
hopejr <hopejr@gmail.com>
2024-06-09 15:51:48 +1000

    Build Targets
    - Fixing build scripts for working on x64 but targeting ARM.

--- arch.mjs
+++ arch.mjs
@@ -1,13 +1,19 @@
 import spawn from "cross-spawn"
+import os from 'os'
 
-if (process.platform === "linux") {
-  spawn.sync("npm", ["run", process.env.npm_config_target_arch === "arm" ? "build:arm" : process.env.npm_config_target_arch === "x64" ? "build:linuxx86" : "build"], {
-    input: "Native module build required.",
-    stdio: "inherit"
-  })
-} else {
-  spawn.sync("npm", ["run", "build"], {
-    input: "Native module build required.",
-    stdio: "inherit"
-  })
+let buildParam = 'build'
+
+if (process.platform === 'linux') {
+  if (os.arch() !== 'arm' && ['armv7l', 'arm'].includes(process.env.npm_config_target_arch)) {
+    buildParam = 'build:linux-arm'
+  } else if (os.arch() !== 'arm64' && process.env.npm_config_target_arch === 'arm64') {
+    buildParam = 'build:linux-arm64'
+  } else if (os.arch() !== 'x64' && process.env.npm_config_target_arch === 'x64') {
+    buildParam = 'build:linux-x64'
+  }
 }
+
+spawn.sync('npm', ['run', buildParam], {
+  input: 'Native module build required.',
+  stdio: 'inherit'
+})
